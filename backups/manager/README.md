fb36f6d8db51745b4f67052bf002ddcb2468100f
Matthieu Flemal <mafl@hms.se>
2026-02-28 10:42:57 +0100

    feat: move backup to dedicated project

--- backups/manager/README.md
+++
@@ -1,9 +0,0 @@
-# Homelab Backup Manager
-
-A unified tool to manage Docker and system backups using Python.
-
-## Usage
-
-```bash
-poetry run homelab-backup run-all
-```
--- backups/manager/src/homelab_backup_manager/main.py
+++
@@ -1,7 +0,0 @@
-"""
-Entry point for the application.
-"""
-from homelab_backup_manager.cli import cli
-
-if __name__ == "__main__":
-    cli() # pylint: disable=no-value-for-parameter
